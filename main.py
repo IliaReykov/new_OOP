4b34125bdf3fc0e628b4027ad35c7fb2e3e9acda
IliaReykov <arhipiv1@gmail.com>
2024-06-07 14:24:14 +0400

    add abstractmethod, made the class Product dependent on Base_Product

--- main.py
+++ main.py
@@ -1,3 +1,97 @@
+from abc import ABC, abstractmethod
+
+
+class Base_Product:
+    @abstractmethod
+    def __init__(self):
+        pass
+
+
+class Product(Base_Product):
+    name: str
+    description: str
+    prod: list
+    price = float
+    amount = int
+
+    def __init__(self, name, description, price, amount):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.amount = amount
+
+    @classmethod
+    def add_product(cls, name, description, price, quantity):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print('Цена введена некорректная')
+        else:
+            self.__price = value
+
+    def __str__(self):
+        return f'Название продукта: {self.name}, Цена: {self.__price}руб. Остаток: {self.amount}шт.'
+
+    def __add__(self, other):
+        return (self.__price * self.amount) + (other.price * other.amount)
+
+    def __len__(self):
+        return sum(product.prod for product in self.__prod)
+        raise TypeError
+
+
+class Phone(Product):
+    perfomance: int
+    model: str
+    storage: int
+    color_phone: str
+
+    @property
+    def price(self):
+        return self.__price
+
+    def __add__(self, other):
+        if type(self) == type(other):
+            return (self.__price * self.amount) + (other.price * other.amount)
+        raise ValueError("Можно складывать только экземпляры этого класса")
+
+    def __init__(self, name, description, price, amount, perfomance, model, storage, color_phone):
+        super().__init__(name, description, price, amount)
+        self.perfomance = perfomance
+        self.model = model
+        self.storage = storage
+        self.color_phone = color_phone
+
+
+class Grass(Product):
+    made_in: str
+    growth_time: int
+    color_grass: str
+
+    def __init__(self, name, description, price, amount, made_in, growth_time, color_grass):
+        super().__init__(name, description, price, amount)
+        self.made_in = made_in
+        self.growth_time = growth_time
+        self.color_grass = color_grass
+
+
+if __name__ == "__main__":
+    phone_1 = Phone("GoodPhone", "Smartphone", 12599, 12, 43000, 40, 16, "Ocean Blue")
+    print(phone_1.__dict__)
+    phone_2 = Phone("WellPhone", "Smartphone", 21999, 40, 99999, 7, 256, "Gold Star")
+    print(phone_2.__dict__)
+    grass_1 = Grass("Sweden Grass", "Grass", 4599, 12, "Sweden", 12, "Green")
+    print(grass_1.__dict__)
+    grass_2 = Grass("Australian Grass", "Grass", 1499, 40, "Australia", 36, "Yellow")
+    print(grass_2.__dict__)
+
+
 class Category:
     name: str
     description: str
@@ -40,85 +134,3 @@ class Category:
         for product in self.__products:
             amt += product.amount
         return amt
-
-class Product:
-    name: str
-    description: str
-    prod: list
-    price = float
-    amount = int
-
-    def __init__(self, name, description, price, amount):
-        self.name = name
-        self.description = description
-        self.__price = price
-        self.amount = amount
-
-    @classmethod
-    def add_product(cls, name, description, price, quantity):
-        return cls(name, description, price, quantity)
-
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, value):
-        if value <= 0:
-            print('Цена введена некорректная')
-        else:
-            self.__price = value
-
-    def __str__(self):
-        return f'Название продукта: {self.name}, Цена: {self.__price}руб. Остаток: {self.amount}шт.'
-
-    def __add__(self, other):
-        return (self.__price * self.amount) + (other.price * other.amount)
-
-
-    def __len__(self):
-        return sum(product.prod for product in self.__prod)
-        raise TypeError
-
-class Phone(Product):
-    perfomance: int
-    model: str
-    storage: int
-    color_phone: str
-
-    @property
-    def price(self):
-        return self.__price
-
-    def __add__(self, other):
-        if type(self) == type(other):
-            return (self.__price * self.amount) + (other.price * other.amount)
-        raise ValueError("Можно складывать только экземпляры этого класса")
-
-    def __init__(self, name, description, price, amount, perfomance, model, storage, color_phone):
-        super().__init__(name, description, price, amount)
-        self.perfomance = perfomance
-        self.model = model
-        self.storage = storage
-        self.color_phone = color_phone
-
-class Grass(Product):
-    made_in: str
-    growth_time: int
-    color_grass: str
-
-    def __init__(self,name, description, price, amount, made_in, growth_time, color_grass):
-        super().__init__(name, description, price, amount)
-        self.made_in = made_in
-        self.growth_time = growth_time
-        self.color_grass = color_grass
-
-if __name__ == "__main__":
-    phone_1 = Phone("GoodPhone", "Smartphone", 12599, 12, 43000, 40, 16, "Ocean Blue")
-    print(phone_1.__dict__)
-    phone_2 = Phone("WellPhone", "Smartphone", 21999, 40, 99999, 7, 256, "Gold Star")
-    print(phone_2.__dict__)
-    grass_1 = Grass("Swedden Grass", "Grass", 4599, 12, "Swedden", 12, "Green")
-    print(grass_1.__dict__)
-    grass_2 = Grass("Australian Grass", "Grass", 1499, 40, "Australia", 36, "Yellow")
-    print(grass_2.__dict__)
